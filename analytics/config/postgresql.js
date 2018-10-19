e6064b514a14045b86d42c97beb0c709e3d83b3d
aymerickvalette <valaymerick@gmail.com>
2018-10-19 18:55:56 +0200

    Move postgres options to separate file

---
+++ analytics/config/postgresql.js
@@ -0,0 +1,9 @@
+module.exports = {
+    host: 'postgres',
+    user: 'docker',
+    password: 'docker',
+    max: 20,
+    idleTimeoutMillis: 30000,
+    connectionTimeoutMillis: 2000,
+    database: 'analytics'
+}
