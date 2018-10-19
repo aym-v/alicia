850144a17ef197fa5437bd99983c2ef568285ce8
aymerickvalette <valaymerick@gmail.com>
2018-10-19 18:56:06 +0200

    Move postgres options to separate file

--- analytics/src/db.js
+++ analytics/src/db.js
@@ -1,14 +1,7 @@
 const { Pool } = require('pg')
+const postgresOptions = require('../config/postgresql');
 
-const pool = new Pool({
-    host: 'postgres',
-    user: 'docker',
-    password: 'docker',
-    max: 20,
-    idleTimeoutMillis: 30000,
-    connectionTimeoutMillis: 2000,
-    database: 'analytics'
-});
+const pool = new Pool(postgresOptions);
 
 module.exports = {
     postgres: (text, params, callback) => pool.query(text, params, callback)
