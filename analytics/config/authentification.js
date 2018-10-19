6a5d7abbd23b9454e69933ea7f35aa4cd2ecacdf
aymerickvalette <valaymerick@gmail.com>
2018-10-19 18:55:42 +0200

    Change app secrets

--- analytics/config/authentification.js
+++ analytics/config/authentification.js
@@ -1,8 +1,11 @@
 module.exports = {
     auth: {
-        salt: 'D2sProY5Tghaxy8FkDEhWAehVVRxoBVW',
+        salt: '2BB80D537B1DA3E38BD30361AA855686BDE0EACD7162FEF6A25FE97BF527A25B',
         alg: 'SHA256',
         iterations: 100000,
         byteLength: 128
+    },
+    token: {
+        secret: '043A718774C572BD8A25ADBEB1BFCD5C0256AE11CECF9F9C3F925D0E52BEAF89'
     }
 }
