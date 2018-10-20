ab88e30d5fafede7dca4e5c051b933fbfb2a6293
aymerickvalette <valaymerick@gmail.com>
2018-10-20 20:48:39 +0200

    Create ads table

--- config/postgres_seed.sql
+++ config/postgres_seed.sql
@@ -3,3 +3,9 @@ CREATE TABLE users (
     username text UNIQUE,
     password_hash text
 );
+CREATE TABLE ads (
+    id uuid PRIMARY KEY,
+    ad_name text,
+    width integer,
+    height integer
+);
