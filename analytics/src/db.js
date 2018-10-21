39e5196e81db9d8d2377ea2c5b6151c809daa9e0
aymerickvalette <valaymerick@gmail.com>
2018-10-22 01:38:51 +0200

    Add elasticsearch database

--- analytics/src/db.js
+++ analytics/src/db.js
@@ -1,8 +1,11 @@
-const { Pool } = require('pg')
+const { Pool } = require('pg');
+const elasticsearch = require('elasticsearch');
 const postgresOptions = require('../config/postgresql');
 
-const pool = new Pool(postgresOptions);
+const postgresPool = new Pool(postgresOptions);
+const elasticClient = new elasticsearch.Client({ hosts: ["http://elasticsearch:9200"] })
 
 module.exports = {
-    postgres: (text, params, callback) => pool.query(text, params, callback)
+    postgres: (text, params, callback) => postgresPool.query(text, params, callback),
+    elastic: query => elasticClient.search(query)
 }
