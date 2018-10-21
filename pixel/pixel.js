3a2695abc648305447402f1dbba1d715bbeca2c7
aymerickvalette <valaymerick@gmail.com>
2018-10-22 01:39:38 +0200

    Minor refractoring

--- pixel/pixel.js
+++ pixel/pixel.js
@@ -5,12 +5,12 @@ const fs = require('fs');
 app.get('/:adId/:event', (req, res) => {
     res.set('Content-Type', 'image/png');
     res.status(201).send(Buffer.from('789c636000020000050001'));
-    const log = { 
-        id: req.params.adId, 
-        event: req.params.event, 
-        parameters: req.query 
+    const log = {
+        id: req.params.adId,
+        event: req.params.event,
+        parameters: req.query
     };
-    const stream = fs.createWriteStream('./logs/events.log', {flags:'a'});
+    const stream = fs.createWriteStream('./logs/events.log', { flags: 'a' });
     stream.write(JSON.stringify(log) + '\n');
     stream.end();
 });
