b28d6bcff33b790f2ccffb0da10e1a5665095970
aymerickvalette <valaymerick@gmail.com>
2018-10-21 14:15:23 +0200

    Add CRUD methods to ad service

--- analytics/src/ad/ad.model.js
+++ analytics/src/ad/ad.model.js
@@ -2,10 +2,23 @@ const db = require('../db');
 const uuid = require('uuid/v4');
 
 const create = async (name, width, height) => {
-    const query = await db.postgres('INSERT INTO ads(id, ad_name, width, height) VALUES ($1, $2, $3, $4)', [uuid(), name, width, height]);
+    const id = uuid();
+    const query = await db.postgres('INSERT INTO ads(id, ad_name, width, height) VALUES ($1, $2, $3, $4)', [id, name, width, height]);
+    return id
+};
+
+const remove = async id => {
+    const query = await db.postgres('DELETE FROM ads WHERE id = $1', [id]);
+    return query;
+};
+
+const update = async (id, name, width, height) => {
+    const query = await db.postgres('UPDATE ads SET ad_name = $1, width = $2, height = $3 WHERE id = $4', [name, width, height, id]);
     return query
 }
 
 module.exports = {
-    create
+    create,
+    remove,
+    update
 }
--- analytics/src/ad/ad.router.js
+++ analytics/src/ad/ad.router.js
@@ -6,12 +6,30 @@ const router = new Router();
 router.post('/ads', async (req, res) => {
     const { name, width, height } = req.body;
     try {
-        await model.create(name, width, height);
-        res.status(200).end();
+        const id = await model.create(name, width, height);
+        res.json({id});
     } catch (err) {
         res.status(500).end();
     }
+});
 
+router.delete('/ads/:id', async (req, res) => {
+    try {
+        const query = await model.remove(req.params.id);
+        res.json(query);
+    } catch (err) {
+        res.status(500).end();
+    }
+});
+
+router.put('/ads/:id', async (req, res) => {
+    const { name, width, height } = req.body;
+    try {
+        const query = await model.update(req.params.id, name, width, height);
+        res.json(query);
+    } catch (err) {
+        res.status(500).end();
+    }
 });
 
 module.exports = router
