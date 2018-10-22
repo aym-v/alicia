6a33a072eb09cd9dff1ea001e0ce2b44bc612298
aymerickvalette <valaymerick@gmail.com>
2018-10-22 20:52:08 +0200

    Aggregate events

--- analytics/src/event/event.model.js
+++ analytics/src/event/event.model.js
@@ -1,8 +1,19 @@
 const db = require('../db');
 
-const selectAll = async id => await db.elastic({
-    index: id
-});
+const selectAll = async id => {
+    const response = await db.elastic({
+        "index": id,
+        "size": 0,
+        "body": {
+            "aggs" : {
+                "events" : {
+                    "terms" : { "field" : "event",  "size" : 500 }
+                }
+            }
+        }
+    });
+    return response.aggregations.events.buckets;
+};
 
 module.exports = {
     selectAll
