cb59f3a9cd69339e568534f931873c0514b0501b
aymerickvalette <valaymerick@gmail.com>
2018-10-24 00:11:41 +0200

    Add logo to Login screen

--- dashboard/src/components/Login/Login.js
+++ dashboard/src/components/Login/Login.js
@@ -1,5 +1,13 @@
 import React from 'react'
+import './Login.css'
 
-const Login = () => <div>Login</div>
+const Login = () => (
+    <div className="login">
+        <div className="login__logo"></div>
+        <div className="login__modal">
+
+        </div>
+    </div>
+)
 
 export default Login
