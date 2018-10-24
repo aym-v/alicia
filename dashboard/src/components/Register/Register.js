e26d8796be1e669b8401325e0b0e56714d3695fc
aymerickvalette <valaymerick@gmail.com>
2018-10-25 00:25:40 +0200

    Prepare register component

--- dashboard/src/components/Register/Register.js
+++ dashboard/src/components/Register/Register.js
@@ -1,5 +1,19 @@
 import React from 'react'
+import './Register.css'
 
-const Login = () => <div>Login</div>
+const Register = () => (
+    <div className="register">
+        <div className="register__logo logo"></div>
+        <div className="register__modal modal">
+            <p className="subtitle">Register</p>
+            <form>  
+                <input autoComplete="off" placeholder="username" className="caption" id="username" type="search"></input>
+                <input placeholder="password" className="caption" id="password" type="password"></input>
+                <input placeholder="secret" className="caption" id="secret" type="password"></input>
+            </form>
+            <button>Create</button>
+        </div>
+    </div>
+)
 
-export default Login
+export default Register
