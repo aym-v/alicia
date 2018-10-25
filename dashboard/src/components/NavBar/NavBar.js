5541313d12a08df9bfd6ae53de0c46d9449d10b7
aymerickvalette <valaymerick@gmail.com>
2018-10-26 01:16:15 +0200

    Style navbar

--- dashboard/src/components/NavBar/NavBar.js
+++ dashboard/src/components/NavBar/NavBar.js
@@ -1,5 +1,21 @@
 import React from 'react'
+import './NavBar.css'
 
-const NavBar = () => <div>NavBar</div>
+const NavBar = () => (
+    <div className="navbar">
+        <div className="navbar__logo">
+            <div className="logo"></div>
+        </div>
+        <div className="navbar__search">
+            <i className="material-icons">search</i>
+            <input className="navbar__input" placeholder="search..." type="search"></input>
+        </div>
+        <div className="navbar__profile">
+            <div className="navbar__username"></div>
+            <div className="navbar__picture"></div>
+            <div className="navbar__dropdown"></div>
+        </div>
+    </div>
+)
 
 export default NavBar
