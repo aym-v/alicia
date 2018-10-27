2948cb206d8ac18efe5d5d1d5287e02c1b1a2cae
aymerickvalette <valaymerick@gmail.com>
2018-10-27 17:26:29 +0200

    Add navbar

--- dashboard/src/components/NavBar/NavBar.js
+++ dashboard/src/components/NavBar/NavBar.js
@@ -1,21 +1,63 @@
-import React from 'react'
+import React, { Component } from 'react'
 import './NavBar.css'
+import Arrow from './Arrow'
 
-const NavBar = () => (
-    <div className="navbar">
-        <div className="navbar__logo">
-            <div className="logo"></div>
-        </div>
-        <div className="navbar__search">
-            <i className="material-icons">search</i>
-            <input className="navbar__input" placeholder="search..." type="search"></input>
-        </div>
-        <div className="navbar__profile">
-            <div className="navbar__username"></div>
-            <div className="navbar__picture"></div>
-            <div className="navbar__dropdown"></div>
-        </div>
-    </div>
-)
+class NavBar extends Component {
+    constructor(props) {
+        super(props);
+        this.state = {
+            showDropDown: false
+        }
+    }
+
+    componentDidUpdate() {
+        if (this.state.showDropDown) {
+            window.addEventListener('click', this.handleGlobalClick)
+        } else {
+            window.removeEventListener('click', this.handleGlobalClick);
+        }
+    }
+
+    triggerDropdown = () => {
+        this.setState(
+            {
+                showDropDown: !this.state.showDropDown
+            }
+        )
+    }
+
+    handleGlobalClick = (event) => {
+        const navbarNode = document.querySelector('.navbar');
+        if (!navbarNode.contains(event.target)) {
+            this.triggerDropdown();
+        }
+    }
+
+
+    render() {
+        const { user = 'Alicia', picture = 'https://randomuser.me/api/portraits/women/90.jpg' } = this.props;
+        return (
+            <div className={`navbar${this.state.showDropDown ? ' dropdown' : ''}`}>
+                <div className="navbar__logo">
+                    <div className="logo"></div>
+                </div>
+                <div className="navbar__search">
+                    <i className="material-icons">search</i>
+                    <input className="navbar__input" placeholder="Search..." type="search"></input>
+                </div>
+                <div onClick={this.triggerDropdown} onBlur={this.triggerDropdown} className="navbar__profile">
+                    <div className="navbar__username">Hi, {user}</div>
+                    <div className="navbar__dropdown">
+                        <img src={picture} alt="profile" className="navbar__picture"></img>
+                        <div className="navbar__arrow"><Arrow></Arrow></div>
+                    </div>
+                </div>
+                <div className="navbar__dropdown-menu">
+                    <div className="navbar__signout"><i className="material-icons">exit_to_app</i>Logout</div>
+                </div>
+            </div>
+        )
+    }
+}
 
 export default NavBar
