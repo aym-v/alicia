33dc25e7f58056a7bd488de56dbe09b3cc7c466a
aymerickvalette <valaymerick@gmail.com>
2018-10-27 17:26:04 +0200

    Use svg for the dropdown arrow

---
+++ dashboard/src/components/NavBar/Arrow.js
@@ -0,0 +1,13 @@
+import React from 'react'
+
+const Ad = () => (
+    <svg width='10' height='5' viewBox='0 0 10 5' xmlns='http://www.w3.org/2000/svg'>
+        <g fill='none' fillRule='evenodd'>
+            <g transform='translate(-1395 -40)' fill='#E0E0E0'>
+                <polygon transform='rotate(-180 1400 42.128)' points='1400 40 1404.2553 44.2553017 1395.7447 44.2553017' />
+            </g>
+        </g>
+    </svg>
+)
+
+export default Ad
