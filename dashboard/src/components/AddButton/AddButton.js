af8f162bd569a2179ca67ccf508c91fa6c34cc66
aymerickvalette <valaymerick@gmail.com>
2018-10-29 00:51:55 +0100

    Add a button component to create new ads

---
+++ dashboard/src/components/AddButton/AddButton.js
@@ -0,0 +1,6 @@
+import React from 'react'
+import './AddButton.css'
+
+const AddButton = () => <div className="add-button">+</div>
+
+export default AddButton
--- dashboard/src/components/App/App.js
+++ dashboard/src/components/App/App.js
@@ -5,6 +5,7 @@ import Ads from '../Ads/Ads'
 import Login from '../Login/Login'
 import Register from '../Register/Register'
 import NavBar from '../NavBar/NavBar'
+import AddButton from '../AddButton/AddButton'
 import './App.css'
 
 class App extends Component {
@@ -15,6 +16,7 @@ class App extends Component {
           <Route path="/login" component={Login} />
           <Route path="/register" component={Register} />
           <Route path="/ads" component={NavBar} />
+          <Route path="/ads" component={AddButton} />
           <Switch>
             <Route path="/ads/:id" component={Ad} />
             <Route path="/ads" component={Ads} />
