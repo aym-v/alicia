d7facdfbefd8752b947ee17bd8e5b42b3f1f856f
aymerickvalette <valaymerick@gmail.com>
2018-11-10 19:10:49 +0100

    Handle actions

--- dashboard/src/reducers/index.js
+++ dashboard/src/reducers/index.js
@@ -1,4 +1,20 @@
+import { AUTHENTICATE, AUTHENTICATED } from '../actions'
+
 const reducer = (previousState, action) => {
+    switch (action.type) {
+        case AUTHENTICATE:
+            return {
+                ...previousState,
+                authenticating: true
+            }
+        case AUTHENTICATED:
+            return {
+                ...previousState,
+                authenticating: false
+            }
+        default:
+            return previousState
+    }
     return previousState
 }
 
