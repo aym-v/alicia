0695437f2b9a7593ee8fdbb0f0d175427bc98e52
Aymerick Valette <valaymerick@gmail.com>
2018-11-10 14:05:51 +0100

    Add rotating animation to Add button

--- dashboard/src/components/AddButton/AddButton.js
+++ dashboard/src/components/AddButton/AddButton.js
@@ -1,6 +1,6 @@
 import React from 'react'
 import './AddButton.css'
 
-const AddButton = () => <div className="add-button">+</div>
+const AddButton = () => <div className="add-button"><div>+</div></div>
 
 export default AddButton
